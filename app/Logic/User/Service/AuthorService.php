88c8f8de85af71eb5839a38092ac34dc9b61028b
7small7 <1005349393@qq.com>
2023-05-02 11:40:56 +0800

    创作者订阅

--- app/Logic/User/Service/AuthorService.php
+++ app/Logic/User/Service/AuthorService.php
@@ -1,5 +1,5 @@
 <?php
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace App\Logic\User\Service;
 
@@ -48,16 +48,16 @@ class AuthorService extends BaseService implements UserServiceInterface
 
     public function servicePublish(array $requestParams): array
     {
-        $userId = $this->getUserUid();
-        $authorId = SnowFlakeId::getId();
+        $userId     = $this->getUserUid();
+        $authorId   = SnowFlakeId::getId();
         $authorInfo = $this->serviceFind(["user_uid" => $userId]);
         if (count($authorInfo) == 0) {
-            $authorInfo = [
-                "uid"          => $authorId,
-                "user_uid"     => $userId,
+            $authorInfo   = [
+                "uid" => $authorId,
+                "user_uid" => $userId,
                 "is_forbidden" => 2,
                 "series_count" => 0,
-                "qr_url"       => "https://img.wxcha.com/m00/50/12/81b6ba3f79a9565ec32bd6d596a99944.jpg",
+                "qr_url" => "https://img.wxcha.com/m00/50/12/81b6ba3f79a9565ec32bd6d596a99944.jpg",
             ];
             $createAuthor = (new AuthorRepository())->repositoryCreate($authorInfo);
             if (!$createAuthor) {
@@ -67,38 +67,38 @@ class AuthorService extends BaseService implements UserServiceInterface
         if ($authorInfo["is_forbidden"] == 1) {
             return ["code" => 1, "msg" => "发布权限已被禁用"];
         }
-        $authorId = $authorInfo["uid"];
+        $authorId   = $authorInfo["uid"];
         $imageArray = json_decode($requestParams["image_list"], true);
-        $imageList = [];
-        $imageUid = SnowFlakeId::getId();
+        $imageList  = [];
+        $imageUid   = SnowFlakeId::getId();
         foreach ($imageArray as $value) {
-            $pathInfo = pathinfo($value);
+            $pathInfo    = pathinfo($value);
             $imageList[] = [
-                "uid"        => SnowFlakeId::getId(),
-                "user_uid"   => $userId,
+                "uid" => SnowFlakeId::getId(),
+                "user_uid" => $userId,
                 "author_uid" => $authorId,
-                "image_uid"  => $imageUid,
-                "url"        => $pathInfo["dirname"],
-                "path"       => $pathInfo["basename"],
-                "download"   => 0,
-                "collect"    => 0,
+                "image_uid" => $imageUid,
+                "url" => $pathInfo["dirname"] . "/",
+                "path" => $pathInfo["basename"],
+                "download" => 0,
+                "collect" => 0,
                 "created_at" => date("Y-m-d H:i:s"),
                 "updated_at" => date("Y-m-d H:i:s"),
             ];
         }
         $image = [
-            "uid"          => $imageUid,
-            "user_uid"     => $userId,
-            "author_uid"   => $authorId,
-            "series_uid"   => $requestParams["image_series_uid"],
+            "uid" => $imageUid,
+            "user_uid" => $userId,
+            "author_uid" => $authorId,
+            "series_uid" => $requestParams["image_series_uid"],
             "category_uid" => $requestParams["image_category_uid"],
-            "item_count"   => count($imageArray),
-            "url"          => $imageList[0]["url"],
-            "path"         => $imageList[0]["path"],
-            "title"        => $requestParams["image_title"],
-            "download"     => 0,
-            "collect"      => 0,
-            "is_show"      => 2,
+            "item_count" => count($imageArray),
+            "url" => $imageList[0]["url"],
+            "path" => $imageList[0]["path"],
+            "title" => $requestParams["image_title"],
+            "download" => 0,
+            "collect" => 0,
+            "is_show" => 2,
         ];
         if ((new ImageRepository())->repositoryCreate(["image" => $image, "image_item" => $imageList])) {
             return ["code" => 0, "msg" => "请等待审核"];
